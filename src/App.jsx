8ca4407deb07e7ce9317bb48eb006eb3bce49c86
Evangel2020 <mayoressceo@gmail.com>
2022-11-07 08:52:07 -0800

    starting out

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heropage from './component/Heropage/index'
+
+function App() {
+
+  return (
+    <div>
+      <h1>App</h1>
+      <Heropage />
+    </div>
+  )
+}
+
+export default App
